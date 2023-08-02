7486393fd3bc00e46b4acd2c0ff42ff7676dae08
Edwin Lock <edwinlock@users.noreply.github.com>
2023-08-02 11:23:40 +0100

    fixed things

--- windows_installer.cmd
+++ windows_installer.cmd
@@ -10,9 +10,9 @@ if not exist "%dir%\NUL" mkdir "%dir%"
 : copy over files
 XCOPY /e /r /y "%CD%\*.*" "%dir%"
 : create 'executable'
-echo @echo off ^&^& julia --project="%HOMEPATH%\.julia\environments\BellusAuction" --sysimage="%dir%\sys_bellus.so" "%dir%\run.jl" %%^* > "%PROGRAMFILES%\bpma.cmd"
+echo @echo off ^&^& julia --project="%HOMEPATH%\.julia\environments\BellusAuction" --sysimage="%dir%\sys_bellus.so" "%dir%\run.jl" %%^* > "%SYSTEMROOT%\bpma.cmd"
 : run Julia install script
-julia --threads auto "%dir%\install.jl"
+: julia --threads auto "%dir%\install.jl"
 : Add program files directory to PATH
 : path|find /i "%dir%" > nul || setx path "%PATH%;%dir%"
 echo "Installation completed"
